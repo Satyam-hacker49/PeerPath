39f74b42ba9e5aae597429ef445485c3c9251d0f
Satyam Sharma <satyamshar978@gmail.com>
2025-06-11 02:33:05 +0530

    PeerPath

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import LoginForm from './LoginForm.jsx';
+import RegistrationForm from './RegistrationForm.jsx';
 
 function App() {
 
   return (
     <>
-    <LoginForm />
+    {/* <LoginForm /> */}
+    <RegistrationForm />
     </>
   )
 }
---
+++ src/RegistrationForm.jsx
@@ -0,0 +1,61 @@
+import "./RegistrationForm.css";
+import PersonIcon from '@mui/icons-material/Person';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+export default function RegistrationForm(){
+
+    return(
+
+        <div className="Registration-form">
+
+            <div className="Registration-Left-part">
+                 <h1>Registration</h1>
+
+                     <div className="regis-social-section">
+        <div className="regis-social-title">Register on</div>
+        <div className="Regis-socialaccount">
+            <a href="#" className="regis-icon-link">
+                <LinkedInIcon style={{ color: 'black', height: "50px", width: "50px" }} />
+            </a>
+            <a href="#" className="regis-icon-link">
+                <GitHubIcon style={{ color: 'black', height: "50px", width: "50px" }} />
+            </a>
+        </div>
+    </div>
+                  {/* <ul className="Regis-socialaccount">
+                    <a href="" >Register on <LinkedInIcon style={{ color: 'black',height:"50px",width:"80px",marginRight:"50px" }}></LinkedInIcon></a>
+                    <a href="" >Register on <GitHubIcon style={{ color: 'black',height:"50px",width:"90px" }}></GitHubIcon></a>
+                  </ul> */}
+
+                 <p>Still Have Issue?</p>
+
+                 <div className="scholarnuminput">
+                    <input type="text" placeholder="Scholar Number" id="scholarNum" ></input>
+                    <div id="scholarNumicondiv"><label htmlFor="scholarNum" ><PersonIcon id="scholaricon" fontSize="large" ></PersonIcon></label>
+                 </div>
+                    
+                 </div>
+                 <div className="IssueInput">
+                    <textarea placeholder="Issue Encountered" id="Issue" ></textarea>
+                 </div>
+
+                 <button className="issuesubmitbtn">Submit Response</button>
+                
+            </div>
+
+            
+            <div className="Registration-Right-part">
+                <div>
+                <h1>Welcome Back!</h1>
+                <p>Already have an account?</p>
+                <button className="loginbtn">Login</button>
+                </div>
+               
+            </div>
+
+            
+
+        </div>
+    );
+}
